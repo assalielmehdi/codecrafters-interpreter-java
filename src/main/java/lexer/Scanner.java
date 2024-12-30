ec9e541875e955c82ba7ba89dbb5a45588501f26
El Mehdi ASSALI <assalielmehdi@gmail.com>
2024-12-30 22:52:59 +0100

    Refactoring

--- src/main/java/lexer/Scanner.java
+++ src/main/java/lexer/Scanner.java
@@ -56,94 +56,89 @@ public class Scanner {
   }
 
   private void scanToken() {
-    var symbol = poll();
-
-    switch (symbol) {
-      case '(' -> this.tokens.add(new Token(Token.Type.LEFT_PAREN, "(", null, line));
-      case ')' -> this.tokens.add(new Token(Token.Type.RIGHT_PAREN, ")", null, line));
-      case '{' -> this.tokens.add(new Token(Token.Type.LEFT_BRACE, "{", null, line));
-      case '}' -> this.tokens.add(new Token(Token.Type.RIGHT_BRACE, "}", null, line));
-      case ',' -> this.tokens.add(new Token(Token.Type.COMMA, ",", null, line));
-      case '.' -> this.tokens.add(new Token(Token.Type.DOT, ".", null, line));
-      case '-' -> this.tokens.add(new Token(Token.Type.MINUS, "-", null, line));
-      case '+' -> this.tokens.add(new Token(Token.Type.PLUS, "+", null, line));
-      case ';' -> this.tokens.add(new Token(Token.Type.SEMICOLON, ";", null, line));
-      case '*' -> this.tokens.add(new Token(Token.Type.STAR, "*", null, line));
+    switch (poll()) {
+      case '(' -> addToken(Token.Type.LEFT_PAREN);
+      case ')' -> addToken(Token.Type.RIGHT_PAREN);
+      case '{' -> addToken(Token.Type.LEFT_BRACE);
+      case '}' -> addToken(Token.Type.RIGHT_BRACE);
+      case ',' -> addToken(Token.Type.COMMA);
+      case '.' -> addToken(Token.Type.DOT);
+      case '-' -> addToken(Token.Type.MINUS);
+      case '+' -> addToken(Token.Type.PLUS);
+      case ';' -> addToken(Token.Type.SEMICOLON);
+      case '*' -> addToken(Token.Type.STAR);
       case '/' -> {
         if (match('/')) {
           while (!atEOF() && peek() != '\n') {
             poll();
           }
         } else {
-          this.tokens.add(new Token(Token.Type.SLASH, "/", null, line));
+          addToken(Token.Type.SLASH);
         }
       }
-
-      case '!' -> this.tokens.add(match('=')
-        ? new Token(Token.Type.BANG_EQUAL, "!=", null, line)
-        : new Token(Token.Type.BANG, "!", null, line));
-      case '=' -> this.tokens.add(match('=')
-        ? new Token(Token.Type.EQUAL_EQUAL, "==", null, line)
-        : new Token(Token.Type.EQUAL, "=", null, line));
-      case '<' -> this.tokens.add(match('=')
-        ? new Token(Token.Type.LESS_EQUAL, "<=", null, line)
-        : new Token(Token.Type.LESS, "<", null, line));
-      case '>' -> this.tokens.add(match('=')
-        ? new Token(Token.Type.GREATER_EQUAL, ">=", null, line)
-        : new Token(Token.Type.GREATER, ">", null, line));
-
-      case '"' -> {
-        while (!atEOF() && peek() != '"') {
-          if (poll() == '\n') {
-            line++;
-          }
-        }
-
-        if (atEOF()) {
-          this.errors.add(String.format("[line %d] Error: Unterminated string.", line));
-          return;
-        }
-
-        poll();
-        var lexeme = source.substring(start + 1, current - 1);
-        this.tokens.add(new Token(Token.Type.STRING, '"' + lexeme + '"', lexeme, line));
-      }
-
-      case '0' -> this.tokens.add(new Token(Token.Type.NUMBER, "0", 0.0, line));
-      case '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
-        while (Character.isDigit(peek())) {
-          poll();
-        }
-
-        if (peek() == '.' && Character.isDigit(peekNext())) {
-          do {
-            poll();
-          } while (Character.isDigit(peek()));
-        }
-
-        var lexeme = source.substring(start, current);
-        this.tokens.add(new Token(Token.Type.NUMBER, lexeme, Double.parseDouble(lexeme), line));
-      }
-
+      case '!' -> addToken(match('=') ? Token.Type.BANG_EQUAL : Token.Type.BANG);
+      case '=' -> addToken(match('=') ? Token.Type.EQUAL_EQUAL : Token.Type.EQUAL);
+      case '<' -> addToken(match('=') ? Token.Type.LESS_EQUAL : Token.Type.LESS);
+      case '>' -> addToken(match('=') ? Token.Type.GREATER_EQUAL : Token.Type.GREATER);
+      case '"' -> parseString();
+      case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> parseNumber();
       case '\n' -> line++;
       case ' ', '\t', '\r' -> {}
+      case char symbol when Character.isAlphabetic(symbol) || symbol == '_' -> parseIdentifier();
+      case char symbol -> this.errors.add(String.format("[line %d] Error: Unexpected character: %s", line, symbol));
+    }
+  }
 
-      default -> {
-        if (Character.isAlphabetic(symbol) || symbol == '_') {
-          while (Character.isAlphabetic(peek()) || Character.isDigit(peek()) || peek() == '_') {
-            poll();
-          }
+  private void parseIdentifier() {
+    while (Character.isAlphabetic(peek()) || Character.isDigit(peek()) || peek() == '_') {
+      poll();
+    }
 
-          var lexeme = source.substring(start, current);
-          var type = keywords.getOrDefault(lexeme, Token.Type.IDENTIFIER);
+    var lexeme = source.substring(start, current);
+    var type = keywords.getOrDefault(lexeme, Token.Type.IDENTIFIER);
 
-          this.tokens.add(new Token(type, lexeme, null, line));
-          return;
-        }
+    this.tokens.add(new Token(type, lexeme, null, line));
+  }
 
-        this.errors.add(String.format("[line %d] Error: Unexpected character: %s", line, symbol));
+  private void parseNumber() {
+    while (Character.isDigit(peek())) {
+      poll();
+    }
+
+    if (peek() == '.' && Character.isDigit(peekNext())) {
+      do {
+        poll();
+      } while (Character.isDigit(peek()));
+    }
+
+    var literal = Double.parseDouble(source.substring(start, current));
+    addToken(Token.Type.NUMBER, literal);
+  }
+
+  private void parseString() {
+    while (!atEOF() && peek() != '"') {
+      if (poll() == '\n') {
+        line++;
       }
     }
+
+    if (atEOF()) {
+      this.errors.add(String.format("[line %d] Error: Unterminated string.", line));
+      return;
+    }
+
+    poll();
+    var literal = source.substring(start + 1, current - 1);
+    addToken(Token.Type.STRING, literal);
+  }
+
+  private void addToken(Token.Type type) {
+    addToken(type, null);
+  }
+
+  private void addToken(Token.Type type, Object literal) {
+    var lexeme = source.substring(start, current);
+    this.tokens.add(new Token(type, lexeme, literal, line));
   }
 
   private boolean atEOF() {
