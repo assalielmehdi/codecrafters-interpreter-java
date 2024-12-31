a931e2982478ffb53559d52aa59327e68c439b47
El Mehdi ASSALI <assalielmehdi@gmail.com>
2024-12-31 01:22:37 +0100

    Parse basics

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,73 @@
 import lexer.Scanner;
+import parser.AstPrinter;
+import parser.Parser;
 
-void main(String[] args) {
-  if (args.length < 2) {
-    System.err.println("Usage: ./your_program.sh <command> <filename>");
-    System.exit(64);
-  }
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-  var command = args[0];
-  var filepath = args[1];
-
-  switch (command) {
-    case "tokenize" -> tokenize(filepath);
-    case "parse" -> scan(filepath);
-    default -> {
-      System.err.println("Unknown command: " + command);
+public class Main {
+  public static void main(String[] args) {
+    if (args.length < 2) {
+      System.err.println("Usage: ./your_program.sh <command> <filename>");
       System.exit(64);
     }
+
+    var command = args[0];
+    var filepath = args[1];
+
+    switch (command) {
+      case "tokenize" -> tokenize(filepath);
+      case "parse" -> scan(filepath);
+      default -> {
+        System.err.println("Unknown command: " + command);
+        System.exit(64);
+      }
+    }
   }
-}
 
-String readFile(String filepath) {
-  var content = "";
-  try {
-    content = Files.readString(Path.of(filepath));
-  } catch (IOException e) {
-    System.err.println("Error reading file: " + e.getMessage());
-    System.exit(65);
+  static String readFile(String filepath) {
+    var content = "";
+    try {
+      content = Files.readString(Path.of(filepath));
+    } catch (IOException e) {
+      System.err.println("Error reading file: " + e.getMessage());
+      System.exit(65);
+    }
+    return content;
   }
-  return content;
-}
 
-void tokenize(String filepath) {
-  var scanner = new Scanner(readFile(filepath));
-  scanner.scan();
+  static void tokenize(String filepath) {
+    var scanner = new Scanner(readFile(filepath));
+    scanner.scan();
 
-  scanner.getTokens().forEach(System.out::println);
+    scanner.getTokens().forEach(System.out::println);
 
-  var errors = scanner.getErrors();
-  errors.forEach(System.err::println);
-
-  if (!errors.isEmpty()) {
+    var errors = scanner.getErrors();
     errors.forEach(System.err::println);
-    System.exit(65);
-  }
-}
 
-void scan(String filepath) {
-  // TODO
+    if (!errors.isEmpty()) {
+      errors.forEach(System.err::println);
+      System.exit(65);
+    }
+  }
+
+  static void scan(String filepath) {
+    var scanner = new Scanner(readFile(filepath));
+    scanner.scan();
+
+    var parser = new Parser(scanner.getTokens());
+    parser.parse();
+
+    var printer = new AstPrinter();
+
+    parser.getExpressions().forEach((expr) -> System.out.println(printer.print(expr)));
+
+    var errors = parser.getErrors();
+    errors.forEach(System.err::println);
+    if (!errors.isEmpty()) {
+      errors.forEach(System.err::println);
+      System.exit(65);
+    }
+  }
 }
---
+++ src/main/java/parser/Parser.java
@@ -0,0 +1,137 @@
+package parser;
+
+import lexer.Token;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+  private final List<Token> tokens;
+  private int current = 0;
+
+  private final List<Expr> expressions = new ArrayList<>();
+  private final List<String> errors = new ArrayList<>();
+
+  public Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
+
+  public List<Expr> getExpressions() {
+    return expressions;
+  }
+
+  public List<String> getErrors() {
+    return errors;
+  }
+
+  public void parse() {
+    while (!atEOF()) {
+      expressions.add(expression());
+    }
+  }
+
+  private Expr expression() {
+    return equality();
+  }
+
+  private Expr equality() {
+    var expr = comparison();
+
+    while (match(Token.Type.BANG_EQUAL, Token.Type.EQUAL_EQUAL)) {
+      var operator = poll();
+      var right = comparison();
+      expr = new Expr.Binary(expr, right, operator);
+    }
+
+    return expr;
+  }
+
+  private Expr comparison() {
+    var expr = term();
+
+    while (match(Token.Type.GREATER, Token.Type.GREATER_EQUAL, Token.Type.LESS, Token.Type.LESS_EQUAL)) {
+      var operator = poll();
+      var right = term();
+      expr = new Expr.Binary(expr, right, operator);
+    }
+
+    return expr;
+  }
+
+  private Expr term() {
+    var expr = factor();
+
+    while (match(Token.Type.MINUS, Token.Type.PLUS)) {
+      var operator = poll();
+      var right = factor();
+      expr = new Expr.Binary(expr, right, operator);
+    }
+
+    return expr;
+  }
+
+  private Expr factor() {
+    var expr = unary();
+
+    while (match(Token.Type.SLASH, Token.Type.STAR)) {
+      var operator = poll();
+      var right = unary();
+      expr = new Expr.Binary(expr, right, operator);
+    }
+
+    return expr;
+  }
+
+  private Expr unary() {
+    if (match(Token.Type.BANG, Token.Type.MINUS)) {
+      poll();
+      return unary();
+    }
+
+    return primary();
+  }
+
+  private Expr primary() {
+    var currentToken = poll();
+
+    return switch (currentToken.type()) {
+      case Token.Type.NUMBER, Token.Type.STRING -> new Expr.Literal(currentToken.literal());
+      case Token.Type.TRUE -> new Expr.Literal(true);
+      case Token.Type.FALSE -> new Expr.Literal(false);
+      case Token.Type.NIL -> new Expr.Literal(null);
+      case Token.Type.LEFT_PAREN -> {
+        var expr = expression();
+
+        if (!match(Token.Type.RIGHT_PAREN)) {
+          // TODO: ERROR
+        }
+
+        yield new Expr.Grouping(expr);
+      }
+      default -> throw new RuntimeException(); // TODO: Error
+    };
+  }
+
+  private boolean atEOF() {
+    return current >= tokens.size() || tokens.get(current).type().equals(Token.Type.EOF);
+  }
+
+  private Token poll() {
+    return tokens.get(current++);
+  }
+
+  private boolean match(Token.Type... types) {
+    if (atEOF()) {
+      return false;
+    }
+
+    var currentType = tokens.get(current).type();
+    for (var type : types) {
+      if (currentType == type) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
