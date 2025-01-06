d8e7b82ca40808573317b65bce33aeef95b99ee1
El Mehdi ASSALI <elmehdi.assali@contentsquare.com>
2025-01-06 01:16:07 +0100

    Support basic expr evaluation

--- src/main/java/parser/Interpreter.java
+++ src/main/java/parser/Interpreter.java
@@ -58,7 +58,10 @@ public class Interpreter implements Visitor<Object> {
 
   @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
-    return expr.literal();
+    return switch (expr.literal()) {
+      case Double d when Math.floor(d) == d -> d.longValue();
+      default -> expr.literal();
+    };
   }
 
   @Override
